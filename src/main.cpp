20a08e613f67a66fd89db323912faa9d27ce11ce
Hydran00 <davide.nardi-1@studenti.unitn.it>
2024-08-28 19:25:34 +0200

    yolo 1

--- src/main.cpp
+++ src/main.cpp
@@ -10,56 +10,61 @@
 #include "json.hpp"
 #include "sensor_msgs/msg/point_cloud2.hpp"
 #include "sensor_msgs/point_cloud2_iterator.hpp"
-
-
+#include "segmentation_srvs/srv/segmentation.hpp"
+// cvbridge
+#include <cv_bridge/cv_bridge.h>
 using namespace sl;
 using namespace std::chrono_literals;
 
-nlohmann::json getJson(sl::Camera& pcamera, sl::Bodies& bodies,
+nlohmann::json getJson(sl::Camera &pcamera, sl::Bodies &bodies,
                        sl::BODY_FORMAT body_format);
-nlohmann::json getJson(sl::Camera& pcamera, sl::Bodies& bodies, int id,
+nlohmann::json getJson(sl::Camera &pcamera, sl::Bodies &bodies, int id,
                        sl::BODY_FORMAT body_format);
 
 nlohmann::json bodyDataToJson(sl::BodyData body);
-void parseArgsMonoCam(int argc, char** argv, InitParameters& param);
+void parseArgsMonoCam(int argc, char **argv, InitParameters &param);
 
-int clip(const int& n, const int& lower, const int& upper) {
+int clip(const int &n, const int &lower, const int &upper)
+{
   return std::max(lower, std::min(n, upper));
 }
 
-int getOCVtype(sl::MAT_TYPE type) {
+int getOCVtype(sl::MAT_TYPE type)
+{
   int cv_type = -1;
-  switch (type) {
-    case MAT_TYPE::F32_C1:
-      cv_type = CV_32FC1;
-      break;
-    case MAT_TYPE::F32_C2:
-      cv_type = CV_32FC2;
-      break;
-    case MAT_TYPE::F32_C3:
-      cv_type = CV_32FC3;
-      break;
-    case MAT_TYPE::F32_C4:
-      cv_type = CV_32FC4;
-      break;
-    case MAT_TYPE::U8_C1:
-      cv_type = CV_8UC1;
-      break;
-    case MAT_TYPE::U8_C2:
-      cv_type = CV_8UC2;
-      break;
-    case MAT_TYPE::U8_C3:
-      cv_type = CV_8UC3;
-      break;
-    case MAT_TYPE::U8_C4:
-      cv_type = CV_8UC4;
-      break;
-    default:
-      break;
+  switch (type)
+  {
+  case MAT_TYPE::F32_C1:
+    cv_type = CV_32FC1;
+    break;
+  case MAT_TYPE::F32_C2:
+    cv_type = CV_32FC2;
+    break;
+  case MAT_TYPE::F32_C3:
+    cv_type = CV_32FC3;
+    break;
+  case MAT_TYPE::F32_C4:
+    cv_type = CV_32FC4;
+    break;
+  case MAT_TYPE::U8_C1:
+    cv_type = CV_8UC1;
+    break;
+  case MAT_TYPE::U8_C2:
+    cv_type = CV_8UC2;
+    break;
+  case MAT_TYPE::U8_C3:
+    cv_type = CV_8UC3;
+    break;
+  case MAT_TYPE::U8_C4:
+    cv_type = CV_8UC4;
+    break;
+  default:
+    break;
   }
   return cv_type;
 }
-cv::Mat slMat2cvMat(sl::Mat& input) {
+cv::Mat slMat2cvMat(sl::Mat &input)
+{
   // Since cv::Mat data requires a uchar* pointer, we get the uchar1 pointer
   // from sl::Mat (getPtr<T>()) cv::Mat and sl::Mat will share a single memory
   // structure
@@ -68,12 +73,16 @@ cv::Mat slMat2cvMat(sl::Mat& input) {
       input.getPtr<sl::uchar1>(MEM::CPU), input.getStepBytes(sl::MEM::CPU));
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
   // Initialize ROS2
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("zed_publisher");
   auto point_cloud_pub =
       node->create_publisher<sensor_msgs::msg::PointCloud2>("point_cloud", 1);
+  auto segmentation_client = node->create_client<segmentation_srvs::srv::Segmentation>("human_mask");
+  // cv bridge
+  auto cv_bridge = std::make_shared<cv_bridge::CvImage>();
 
   Camera zed;
   InitParameters init_parameters;
@@ -86,16 +95,18 @@ int main(int argc, char** argv) {
   parseArgsMonoCam(argc, argv, init_parameters);
 
   auto returned_state = zed.open(init_parameters);
-  if (returned_state != ERROR_CODE::SUCCESS) {
+  if (returned_state != ERROR_CODE::SUCCESS)
+  {
     zed.close();
     return EXIT_FAILURE;
   }
 
   PositionalTrackingParameters positional_tracking_parameters;
-  positional_tracking_parameters.set_floor_as_origin = true;
+  // positional_tracking_parameters.set_floor_as_origin = true;
 
   returned_state = zed.enablePositionalTracking(positional_tracking_parameters);
-  if (returned_state != ERROR_CODE::SUCCESS) {
+  if (returned_state != ERROR_CODE::SUCCESS)
+  {
     zed.close();
     return EXIT_FAILURE;
   }
@@ -108,7 +119,8 @@ int main(int argc, char** argv) {
       BODY_TRACKING_MODEL::HUMAN_BODY_ACCURATE;
   body_tracker_params.enable_segmentation = true;
   returned_state = zed.enableBodyTracking(body_tracker_params);
-  if (returned_state != ERROR_CODE::SUCCESS) {
+  if (returned_state != ERROR_CODE::SUCCESS)
+  {
     zed.close();
     return EXIT_FAILURE;
   }
@@ -123,7 +135,7 @@ int main(int argc, char** argv) {
 
   BodyTrackingRuntimeParameters body_tracker_parameters_rt;
   body_tracker_parameters_rt.detection_confidence_threshold = 50;
-  body_tracker_parameters_rt.minimum_keypoints_threshold = 7;
+  // body_tracker_parameters_rt.minimum_keypoints_threshold = 7;
 
   Bodies bodies;
 
@@ -149,22 +161,33 @@ int main(int argc, char** argv) {
 
   cv::Mat image;
   auto point_cloud_msg = std::make_shared<sensor_msgs::msg::PointCloud2>();
-  while (rclcpp::ok() && run) {
+  while (rclcpp::ok() && run)
+  {
     auto err = zed.grab(rt_params);
-    if (err == ERROR_CODE::SUCCESS) {
+    if (err == ERROR_CODE::SUCCESS)
+    {
       sl::Mat sl_image;
       zed.retrieveImage(sl_image, VIEW::LEFT);
 
       cv::Mat cvImage(sl_image.getHeight(), sl_image.getWidth(),
                       (sl_image.getChannels() == 1) ? CV_8UC1 : CV_8UC4,
                       sl_image.getPtr<sl::uchar1>(sl::MEM::CPU));
+
+      if (cvImage.channels() == 4)
+      {
+        cv::Mat tmpImage;
+        cv::cvtColor(cvImage, tmpImage, cv::COLOR_BGRA2BGR);
+        cvImage = tmpImage;
+      }
       // draw bbox
-      cv::Mat resized = cvImage.clone();
-      cv::resize(resized, resized, cv::Size(), 0.5, 0.5);
-      cv::imshow("video", resized);
-      cv::waitKey(1);
+      // cv::Mat resized = cvImage.clone();
+      // cv::resize(resized, resized, cv::Size(), 0.5, 0.5);
+      // cv::imwrite("image_res.png", resized);
+      // cv::imshow("video", resized);
+      // cv::waitKey(1);
       zed.retrieveBodies(bodies, body_tracker_parameters_rt);
-      if(bodies.body_list.size() == 0) {
+      if (bodies.body_list.size() == 0)
+      {
         continue;
       }
 
@@ -172,12 +195,16 @@ int main(int argc, char** argv) {
       viewer.updateData(bodies, cam_pose.pose_data);
 #endif
 
-      if (bodies.is_new) {
-        try {
+      if (bodies.is_new)
+      {
+        try
+        {
           double distance_to_camera = 1000000000000;
           int closest_body = 0;
-          for (int i = 0; i < bodies.body_list.size(); i++) {
-            if (bodies.body_list[i].position.z < distance_to_camera) {
+          for (int i = 0; i < bodies.body_list.size(); i++)
+          {
+            if (bodies.body_list[i].position.z < distance_to_camera)
+            {
               distance_to_camera = bodies.body_list[i].position.z;
               closest_body = i;
             }
@@ -193,13 +220,39 @@ int main(int argc, char** argv) {
           sl::Mat point_cloud;
           zed.retrieveMeasure(point_cloud, sl::MEASURE::XYZRGBA);
 
-          auto mask = bodies.body_list[closest_body].mask;
+          // convert cvImage to uint8_t using opencv
+          cv::Mat image_req = cvImage.clone();
+          // cv::imwrite("image_req.png", image_req);
+          // get mask from service
+          auto request = std::make_shared<segmentation_srvs::srv::Segmentation::Request>();
+          std_msgs::msg::Header header;
+          header.stamp = node->now();
 
-          // if mask is empty, skip
-          if (mask.getWidth() == 0 || mask.getHeight() == 0) {
-            std::cout << "Mask is empty, skip" << std::endl;
-            continue;
+          auto ros_image = cv_bridge::CvImage(header, sensor_msgs::image_encodings::RGB8, image_req);
+          sensor_msgs::msg::Image img_msg;
+          ros_image.toImageMsg(img_msg);
+
+          request->req = img_msg;
+          // //sanity check -> reconstruct image from cvImage
+          // auto cvImage_reconstructed = cv_bridge::toCvCopy(ros_image, "bgr8")->image;
+          // // to rgb
+          // cv::imwrite("image_reconstructed.png", cvImage_reconstructed);
+
+          auto result = segmentation_client->async_send_request(request);
+          int count = 0;
+          while (rclcpp::ok() && result.wait_for(1s) == std::future_status::timeout)
+          {
+            rclcpp::spin_some(node);
+            std::cout << "Waiting for mask" << std::endl;
+            count++;
+            if (count > 4)
+            {
+              rclcpp::shutdown();
+            }
           }
+          cv::Mat mask = cv_bridge::toCvCopy(result.get()->res, "mono8")->image;
+
+          // cv::imshow("mask", mask);
 
           // Bounding box coordinate
           int bb_x_min = bodies.body_list[closest_body].bounding_box_2d[0][0];
@@ -249,53 +302,69 @@ int main(int argc, char** argv) {
                                      ros_pointcloud.height);
           ros_pointcloud.is_dense = false;
 
-          float* data = reinterpret_cast<float*>(ros_pointcloud.data.data());
+          float *data = reinterpret_cast<float *>(ros_pointcloud.data.data());
           sl::float4 point3D;
 
-
- 
-          sl::uchar1 mask_value;
+          // sl::uchar1 mask_value;
           int index = 0;
-          for (int y = bb_y_min; y < bb_y_max; y++) {
-            for (int x = bb_x_min; x < bb_x_max; x++) {
+          for (int y = bb_y_min; y < bb_y_max; y++)
+          {
+            for (int x = bb_x_min; x < bb_x_max; x++)
+            {
               // get the mask value of the pixel
-              if (mask.getValue(x - bb_x_min, y - bb_y_min, &mask_value) ==
-                  ERROR_CODE::SUCCESS) {
-                if (int(mask_value) != 255) {
-                  continue;
-                } else {
-                  // mask original image pixel
-                  cv::Vec4b& pixel = cvImage.at<cv::Vec4b>(y, x);
-                  pixel[0] = 0;
-                  pixel[1] = 0;
-                  pixel[2] = 255;
-                  point_cloud.getValue(x, y, &point3D);
-                  if (index < ros_pointcloud.width * ros_pointcloud.height) {
-                    data[index * 4 + 0] = point3D.x / 1000.0;
-                    data[index * 4 + 1] = point3D.y / 1000.0;
-                    data[index * 4 + 2] = point3D.z / 1000.0;
-                    uint32_t rgb = *reinterpret_cast<uint32_t*>(&point3D.w);
-                    // convert from ABGR to RGBA
-                    rgb = ((rgb & 0x000000FF) << 16) |
-                          ((rgb & 0x0000FF00)) | ((rgb & 0x00FF0000) >> 16);
-                    std::memcpy(&data[index * 4 + 3], &rgb, 4);
-                    index++;
-                  }
+              // if (mask.getValue(x - bb_x_min, y - bb_y_min, &mask_value) ==
+              //     ERROR_CODE::SUCCESS) {
+              //   if (int(mask_value) != 255) {
+              //     continue;
+              //   }
+              if (mask.at<uchar>(y, x) != 255)
+              {
+                continue;
+              }
+              else
+              {
+                // mask original image pixel
+                cv::Vec4b &pixel = cvImage.at<cv::Vec4b>(y, x);
+                pixel[0] = 0;
+                pixel[1] = 0;
+                pixel[2] = 255;
+                point_cloud.getValue(x, y, &point3D);
+                if (index < ros_pointcloud.width * ros_pointcloud.height)
+                {
+                  data[index * 4 + 0] = point3D.x / 1000.0;
+                  data[index * 4 + 1] = point3D.y / 1000.0;
+                  data[index * 4 + 2] = point3D.z / 1000.0;
+                  uint32_t rgb = *reinterpret_cast<uint32_t *>(&point3D.w);
+                  // convert from ABGR to RGBA
+                  rgb = ((rgb & 0x000000FF) << 16) |
+                        ((rgb & 0x0000FF00)) | ((rgb & 0x00FF0000) >> 16);
+                  std::memcpy(&data[index * 4 + 3], &rgb, 4);
+                  index++;
                 }
               }
             }
           }
+          // draw bounding box
+          cv::rectangle(cvImage, cv::Point(bb_x_min, bb_y_min),
+                        cv::Point(bb_x_max, bb_y_max), cv::Scalar(255, 0, 0),
+                        3);
           cv::resize(cvImage, cvImage, cv::Size(), 0.5, 0.5);
           cv::imshow("video", cvImage);
           cv::waitKey(1);
           point_cloud_pub->publish(ros_pointcloud);
-        } catch (SocketException& e) {
+        }
+        catch (SocketException &e)
+        {
           std::cerr << e.what() << std::endl;
         }
       }
-    } else if (err == sl::ERROR_CODE::END_OF_SVOFILE_REACHED) {
+    }
+    else if (err == sl::ERROR_CODE::END_OF_SVOFILE_REACHED)
+    {
       zed.setSVOPosition(0);
-    } else {
+    }
+    else
+    {
     }
 
 #if DISPLAY_OGL
@@ -338,8 +407,9 @@ int main(int argc, char** argv) {
 // }
 
 // Create the json sent to the clients
-nlohmann::json getJson(sl::Camera& pcamera, sl::Bodies& bodies,
-                       sl::BODY_FORMAT body_format) {
+nlohmann::json getJson(sl::Camera &pcamera, sl::Bodies &bodies,
+                       sl::BODY_FORMAT body_format)
+{
   Pose camp;
   pcamera.getPosition(camp);
   nlohmann::json j;
@@ -354,7 +424,8 @@ nlohmann::json getJson(sl::Camera& pcamera, sl::Bodies& bodies,
   bodyData["nb_object"] = bodies.body_list.size();
   bodyData["body_list"] = nlohmann::json::array();
 
-  for (auto& body : bodies.body_list) {
+  for (auto &body : bodies.body_list)
+  {
     bodyData["body_list"].push_back(bodyDataToJson(body));
   }
 
@@ -364,8 +435,9 @@ nlohmann::json getJson(sl::Camera& pcamera, sl::Bodies& bodies,
 }
 
 // send one skeleton at a time
-nlohmann::json getJson(sl::Camera& pcamera, sl::Bodies& bodies, int id,
-                       sl::BODY_FORMAT body_format) {
+nlohmann::json getJson(sl::Camera &pcamera, sl::Bodies &bodies, int id,
+                       sl::BODY_FORMAT body_format)
+{
   Pose camp;
   pcamera.getPosition(camp);
   /// std::cout << "camera position : " << camp.getTranslation() << std::endl;
@@ -382,7 +454,8 @@ nlohmann::json getJson(sl::Camera& pcamera, sl::Bodies& bodies, int id,
   bodyData["nb_object"] = bodies.body_list.size();
   bodyData["body_list"] = nlohmann::json::array();
 
-  if (id < bodies.body_list.size()) {
+  if (id < bodies.body_list.size())
+  {
     bodyData["body_list"].push_back(bodyDataToJson(bodies.body_list[id]));
   }
 
@@ -392,7 +465,8 @@ nlohmann::json getJson(sl::Camera& pcamera, sl::Bodies& bodies, int id,
 }
 
 // If the sender encounter NaN values, it sends 0 instead.
-nlohmann::json bodyDataToJson(sl::BodyData body) {
+nlohmann::json bodyDataToJson(sl::BodyData body)
+{
   nlohmann::json res;
 
   res["id"] = body.id;
@@ -411,7 +485,8 @@ nlohmann::json bodyDataToJson(sl::BodyData body) {
 
   res["confidence"] = isnan(body.confidence) ? 0 : body.confidence;
   res["bounding_box"] = nlohmann::json::array();
-  for (auto& i : body.bounding_box) {
+  for (auto &i : body.bounding_box)
+  {
     nlohmann::json e;
     e["x"] = isnan(i.x) ? 0 : i.x / 1000;
     e["y"] = isnan(i.y) ? 0 : i.y / 1000;
@@ -427,7 +502,8 @@ nlohmann::json bodyDataToJson(sl::BodyData body) {
       isnan(body.dimensions.z) ? 0 : body.dimensions.z / 1000;
 
   res["keypoint"] = nlohmann::json::array();
-  for (auto& i : body.keypoint) {
+  for (auto &i : body.keypoint)
+  {
     nlohmann::json e;
     e["x"] = isnan(i.x) ? 0 : i.x / 1000;
     e["y"] = isnan(i.y) ? 0 : i.y / 1000;
@@ -436,11 +512,13 @@ nlohmann::json bodyDataToJson(sl::BodyData body) {
   }
 
   res["keypoint_confidence"] = nlohmann::json::array();
-  for (auto& i : body.keypoint_confidence) {
+  for (auto &i : body.keypoint_confidence)
+  {
     res["keypoint_confidence"].push_back(isnan(i) ? 0 : i);
   }
   res["local_position_per_joint"] = nlohmann::json::array();
-  for (auto& i : body.local_position_per_joint) {
+  for (auto &i : body.local_position_per_joint)
+  {
     nlohmann::json e;
     e["x"] = isnan(i.x) ? 0 : i.x / 1000;
     e["y"] = isnan(i.y) ? 0 : i.y / 1000;
@@ -448,7 +526,8 @@ nlohmann::json bodyDataToJson(sl::BodyData body) {
     res["local_position_per_joint"].push_back(e);
   }
   res["local_orientation_per_joint"] = nlohmann::json::array();
-  for (auto& i : body.local_orientation_per_joint) {
+  for (auto &i : body.local_orientation_per_joint)
+  {
     nlohmann::json e;
     e["x"] = isnan(i.x) ? 42 : i.x;
     e["y"] = isnan(i.y) ? 42 : i.y;
@@ -473,35 +552,50 @@ nlohmann::json bodyDataToJson(sl::BodyData body) {
 }
 
 // Parse command line arguments for mono-camera.
-void parseArgsMonoCam(int argc, char** argv, InitParameters& param) {
-  if (argc > 1 && string(argv[1]).find(".svo") != string::npos) {
+void parseArgsMonoCam(int argc, char **argv, InitParameters &param)
+{
+  if (argc > 1 && string(argv[1]).find(".svo") != string::npos)
+  {
     // SVO input mode
     param.input.setFromSVOFile(argv[1]);
     cout << "[Sample] Using SVO File input: " << argv[1] << endl;
-  } else if (argc > 1 && string(argv[1]).find(".svo") == string::npos) {
+  }
+  else if (argc > 1 && string(argv[1]).find(".svo") == string::npos)
+  {
     string arg = string(argv[1]);
     unsigned int a, b, c, d, port;
-    if (sscanf(arg.c_str(), "%u.%u.%u.%u:%d", &a, &b, &c, &d, &port) == 5) {
+    if (sscanf(arg.c_str(), "%u.%u.%u.%u:%d", &a, &b, &c, &d, &port) == 5)
+    {
       // Stream input mode - IP + port
       string ip_adress = to_string(a) + "." + to_string(b) + "." +
                          to_string(c) + "." + to_string(d);
       param.input.setFromStream(String(ip_adress.c_str()), port);
       cout << "[Sample] Using Stream input, IP : " << ip_adress
            << ", port : " << port << endl;
-    } else if (sscanf(arg.c_str(), "%u.%u.%u.%u", &a, &b, &c, &d) == 4) {
+    }
+    else if (sscanf(arg.c_str(), "%u.%u.%u.%u", &a, &b, &c, &d) == 4)
+    {
       // Stream input mode - IP only
       param.input.setFromStream(String(argv[1]));
       cout << "[Sample] Using Stream input, IP : " << argv[1] << endl;
-    } else if (arg.find("HD2K") != string::npos) {
+    }
+    else if (arg.find("HD2K") != string::npos)
+    {
       param.camera_resolution = RESOLUTION::HD2K;
       cout << "[Sample] Using Camera in resolution HD2K" << endl;
-    } else if (arg.find("HD1080") != string::npos) {
+    }
+    else if (arg.find("HD1080") != string::npos)
+    {
       param.camera_resolution = RESOLUTION::HD1080;
       cout << "[Sample] Using Camera in resolution HD1080" << endl;
-    } else if (arg.find("HD720") != string::npos) {
+    }
+    else if (arg.find("HD720") != string::npos)
+    {
       param.camera_resolution = RESOLUTION::HD720;
       cout << "[Sample] Using Camera in resolution HD720" << endl;
-    } else if (arg.find("VGA") != string::npos) {
+    }
+    else if (arg.find("VGA") != string::npos)
+    {
       param.camera_resolution = RESOLUTION::VGA;
       cout << "[Sample] Using Camera in resolution VGA" << endl;
     }
